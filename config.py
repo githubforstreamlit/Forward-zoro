ea2a271a355b0830faaff7755ae4f21f699a20c6
githubforstreamlit <136007419+githubforstreamlit@users.noreply.github.com>
2024-02-19 23:20:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from os import environ
 class Config:
     API_ID = int(environ.get("API_ID", 6435225))
     API_HASH = environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "6916847586:AAGzhQhihHIoKJFw3wdpqh4izLw5PRRDF48") 
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://heroku123456abc:wcadzyz8s6@cluster0.ngkzc2f.mongodb.net/")
     DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6172502166').split()]
     
 class temp(object): 
     lock = {}
